4fcb76b2e57ca4fc0bb5880193b492b4b822fbac
Tom <thomas.brown1@digital.hmrc.gov.uk>
2021-03-26 16:33:48 +0000

    Initial commit of Matrices flow

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,13 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+// Comments route
+router.get('/comments', (req, res) => {
+    res.redirect('/matrix_hereditaments_comment');
+
+});
+// Price route
+router.get('/price', (req, res) => {
+    res.redirect('/matrix_hereditaments_price');
+});
